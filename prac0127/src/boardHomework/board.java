64af2b310171217168d217d18522cb39bc917b9e
User <User@211-16>
2023-02-01 13:45:05 +0900

    일부 공유

---
+++ prac0127/src/boardHomework/board.java
@@ -0,0 +1,35 @@
+package boardHomework;
+
+/**
+ * board Model
+ * @author Yn
+ *
+ */
+public class board {
+	//private int total;
+	private String title;
+	private String writer;
+	private int views;
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public int getViews() {
+		return views;
+	}
+	public void setViews(int views) {
+		this.views = views;
+	}
+
+	
+	
+
+}
---
+++ prac0130/src/list_address_book/Address.java
@@ -0,0 +1,30 @@
+package list_address_book;
+
+/**
+ * 주소관리 클래스
+ * @author Yn
+ *
+ */
+public class Address {
+	private String name;
+	private String phoneNumber;
+	private String address;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+}
